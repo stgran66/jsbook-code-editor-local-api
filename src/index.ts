24e00e846fb4c1a9cb996568a22cc09b62ffe0eb
stgran66 <stanislaw.grygoriew@gmail.com>
2023-04-13 12:56:24 +0200

    added two ways of running local-api, either on proxy or resolve path from users machine

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
 import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import path from 'path';
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  app.use(express.static('../../local-client/build'));
-  // app.use(
-  //   createProxyMiddleware({
-  //     target: 'http://127.0.0.1:3000',
-  //     ws: true,
-  //     logLevel: 'silent',
-  //   })
-  // );
+  if (useProxy) {
+    app.use(express.static('./node_modules/local-client/build/index.html'));
+    app.use(
+      createProxyMiddleware({
+        target: 'http://127.0.0.1:3000',
+        ws: true,
+        logLevel: 'silent',
+      })
+    );
+  } else {
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
